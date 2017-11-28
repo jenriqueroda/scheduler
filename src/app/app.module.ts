a464fced671a02991a0900479680e6c3a5283bf6
jeroda <jenriqueroda@gmail.com>
2017-11-28 09:49:24 +0100

    Nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LeftMenuComponent } from './layout/left-menu/left-menu.component';
+import { MainPageComponent } from './content/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftMenuComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
